0ed9fa8c720216150b1934d40aea47af1cef6590
Minsung Joh <jms5049@naver.com>
2018-04-26 22:14:45 +0900

    Update Main.java

--- File Input and Output/src/Main.java
+++ File Input and Output/src/Main.java
@@ -3,42 +3,36 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[]) {
-		String fileName = "phonebook.txt"; /* 읽어올 txt파일 이름 */
-		int size = 100; /* 객체 배열의 크기 */
+		String fileName = "phonebook.txt";
+		int size = 100;
 
 		Run mRun = new Run(fileName, size);
-		
-		// Search 메소드를 불러옴//
-		
+	
 		Scanner aScanner = new Scanner(System.in);
 		String userinputname = null;
 
-		System.out.print("이름을 입력하시오: ");
+		System.out.print("Type Your Name: ");
 		userinputname = aScanner.next();
 
-		System.out.println(userinputname + "의 전화번호는 " + mRun.search(userinputname) + " 입니다.");
-		
-		//Update 메소드를 불러옴//
+		System.out.println(userinputname + "'s phone number is " + mRun.search(userinputname));
 		
 		String newuserinputname = null;
 		String newuserinputnumber = null;
 		
 		
-		System.out.print("이름을 입력하시오: ");
+		System.out.print("Type your name: ");
 		newuserinputname = aScanner.next();
-		System.out.print("전화번호를 입력하시오: ");
+		System.out.print("Type your phone number: ");
 		newuserinputnumber = aScanner.next();
 
 		mRun.update(newuserinputname, newuserinputnumber);
 		
-		System.out.print("이름을 입력하시오: ");
+		System.out.print("Type your name: ");
 		newuserinputname = aScanner.next();
-		System.out.print("전화번호를 입력하시오: ");
+		System.out.print("Type your phone number: ");
 		newuserinputnumber = aScanner.next();
 		
 		mRun.update(newuserinputname, newuserinputnumber);
-				
-		//print 메소드를 불러옴//
 		
 		mRun.print_to_file();
 
