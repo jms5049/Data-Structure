bef0bbf208a4fc5a1f3b033760c3cffba1a96efc
minsung Joh <jms5049@naver.com>
2018-05-02 02:17:07 +0900

    WK10-05

--- Binary and Ordered Tree/OrderedTree.java
+++ Binary and Ordered Tree/OrderedTree.java
@@ -15,6 +15,18 @@ public class OrderedTree {
 		size = 1;
 	}
 
+	public OrderedTree(Object root, List<Object> trees) {
+		this(root);
+		for (Iterator it = trees.iterator(); it.hasNext();) {
+			Object object = it.next();
+			if (object instanceof OrderedTree) {
+				OrderedTree tree = (OrderedTree) object;
+				subtrees.add(tree);
+				size += tree.size();
+			}
+		}
+	}
+
 	public int size() {
 		return size;
 	}
