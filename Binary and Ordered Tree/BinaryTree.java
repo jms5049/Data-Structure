e81aad5d45ef32c45f867be672d47bed10058a5c
minsung Joh <jms5049@naver.com>
2018-05-01 19:45:09 +0900

    WK10-01

---
+++ Binary and Ordered Tree/BinaryTree.java
@@ -0,0 +1,24 @@
+﻿import java.util.*;
+
+public class BinaryTree {
+	private Object root;
+	private BinaryTree left, right;
+
+	public BinaryTree(Object root) {
+		this.root = root;
+		this.left = null; // 자식이 없는 empty tree
+		this.right = null; // 자식이 없는 empty tree
+	}
+
+	public BinaryTree(Object root, BinaryTree left, BinaryTree right) {
+		this.root = root;
+		this.left = left;
+		this.right = right;
+
+	}
+
+	public Object getRoot() {
+		return root;
+	}
+
+}
