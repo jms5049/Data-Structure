65ac5da916c81204e452644c8435c554ada827b0
minsung Joh <jms5049@naver.com>
2018-05-02 02:16:47 +0900

    WK10-04

---
+++ Binary and Ordered Tree/OrderedTree.java
@@ -0,0 +1,22 @@
+﻿import java.util.*;
+
+public class OrderedTree {
+	private Object root;
+	private List<Object> subtrees;
+	private int size;
+
+	public OrderedTree() {
+
+	}
+
+	public OrderedTree(Object root) {
+		this.root = root;
+		subtrees = new LinkedList<Object>();
+		size = 1;
+	}
+
+	public int size() {
+		return size;
+	}
+
+}
