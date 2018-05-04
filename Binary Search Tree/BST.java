690bb5e3de388e032de83b41f5393fbaa853d16f
minsung Joh <jms5049@naver.com>
2018-05-04 18:42:55 +0900

    WK11-01

---
+++ Binary Search Tree/BST.java
@@ -0,0 +1,22 @@
+﻿
+public class BST {
+	private Comparable key;
+	private BST left, right;
+	private BST root;
+	private int rHeight;
+	private int lHeight;
+
+	private int leftSubtreeHeight;
+	private int rightSubtreeHeight;
+
+	public BST() {
+		this.root = this;
+	}
+
+	public BST(Comparable key) {
+		this.key = key;
+		this.leftSubtreeHeight = 0;
+		this.rightSubtreeHeight = 0;
+	}
+
+}
