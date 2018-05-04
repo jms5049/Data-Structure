e0d89bfc7d759791d0a853ce2e092e6f36fd6adb
minsung Joh <jms5049@naver.com>
2018-05-04 18:44:10 +0900

    WK11-02

--- Binary Search Tree/BST.java
+++ Binary Search Tree/BST.java
@@ -19,4 +19,23 @@ public class BST {
 		this.rightSubtreeHeight = 0;
 	}
 
+	public void insert(Object key) {
+
+		if (this.key == null) {
+			this.key = (Comparable) key;
+
+		} else {
+			if (this.key.compareTo((Comparable) key) < 0) {	//입력받은 키값과 루트의 키값과 비교해서 입력받은 키값이 더 크다면
+				if (this.right == null) {	//만약 루트 노드의 오른쪽자식이 없다면
+					this.right = new BST((Comparable) key);	//입력받은 키 값을 가진 노드를 오른쪽 자식으로 붙여준다.
+
+				} else {
+					this.right.insert(key);	//루트 노드보다 크고 루트 노드에 오른쪽 자식이 잇다면, 그 오른쪽 자식과 입력받은 key 값과 비교를 해서 어디에 넣을 지를 알아내기 위해 재귀함수를 쓴다.
+				}
+
+			} 
+
+		}
+	}
+
 }
