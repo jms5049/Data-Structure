29cc1549d187a910677937db683014f7219cb32d
minsung Joh <jms5049@naver.com>
2018-05-15 21:15:05 +0900

    WK13-01

---
+++ Graph using Adjacent List/Graph.java
@@ -0,0 +1,29 @@
+﻿
+public class Graph {
+	int size;
+	int indexNumber;
+	String[] array; // 스트링 배열
+	Node[] nodeArray; // 노드 배열
+	int p, q;
+
+	public Graph(String[] args) {
+		size = args.length;
+		array = new String[size]; 
+		System.arraycopy(args, 0, array, 0, size); 
+													// 그대로 복사한다.
+		nodeArray = new Node[size];
+		for (int i = 0; i < size; i++) { 
+			nodeArray[i] = new Node(i, null);
+		}
+	}
+
+	private class Node {
+		int to;
+		Node next;
+
+		Node(int to, Node next) {
+			this.to = to;
+			this.next = next;
+		}
+	}
+}
