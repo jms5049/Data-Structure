e97e79e6d259a4cfbd9fa4f605bcaf92fd1a8c87
minsung Joh <jms5049@naver.com>
2018-06-13 16:39:32 +0900

    Huffman-02

--- Huffman/HeapPriorityQueue.java
+++ Huffman/HeapPriorityQueue.java
@@ -9,5 +9,45 @@ package huffmantree;public class HeapPriorityQueue implements PriorityQueue{
 		// TODO Auto-generated constructor stub
 		a = new Comparable[capacity];
 	}
+	@Override
+	public void add(Object object) {
+		// TODO Auto-generated method stub
+		if(!(object instanceof Comparable))
+			throw new IllegalArgumentException();
+		Comparable x = (Comparable)object;
+		if(size == a.length)
+ 			resize();
+		int i = size++;
+		while(i>0){
+			int j =i;
+			i = (i-1)/2;
+			if(a[i].compareTo(x)>=0){
+				a[j] = x;
+				return;
+			}
+			a[j] = a[i];
+		}
+		a[i]=x;
+	}
+	@Override
+	public Object best() {
+		// TODO Auto-generated method stub
+		if(size==0)
+ 			throw new java.util.NoSuchElementException();
+		return a[0];
+	}
+	@Override
+	public Object removeBest() {
+		// TODO Auto-generated method stub
+		Object best = best();
+		a[0] = a[--size];
+		heapify(0,size);
+		return best;
+	}
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return size;
+	}		
 	
 }
