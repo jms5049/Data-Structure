5fed8f56e67913efe94345b0f9a669a75151c3f9
minsung Joh <jms5049@naver.com>
2018-06-13 16:39:14 +0900

    Huffman-01

---
+++ Huffman/HeapPriorityQueue.java
@@ -0,0 +1,13 @@
+package huffmantree;public class HeapPriorityQueue implements PriorityQueue{
+	private static final int CAPACITY = 100;
+	private Comparable[] a;
+	private int size;
+	public HeapPriorityQueue(){
+		this(CAPACITY);	
+	}	
+	public HeapPriorityQueue(int capacity) {
+		// TODO Auto-generated constructor stub
+		a = new Comparable[capacity];
+	}
+	
+}
