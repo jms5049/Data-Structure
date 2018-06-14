2ef0243d174c9c73d1040967f204b3074506e26d
minsung Joh <jms5049@naver.com>
2018-06-14 16:40:58 +0900

    Huffman-04

---
+++ Huffman/PriorityQueue.java
@@ -0,0 +1,14 @@
+package huffmantree;
+public interface PriorityQueue {
+		public void add(Object object);
+	//POSTCONDITION: the given object is in this Queue;
+		public Object best();
+	//RETURN: the highest priority element in this Queue;
+	//PRECONDITION: this Queue is not empty;
+		public Object removeBest();
+	//RETURN: the highest priority element in this Queue;
+	//PRECONDITION: this Queue is not empty;
+	//POSTCONDITION: the returned object is not in this Queue;
+		public int size();
+	//RETURN: the number of elements in this Queue;
+	}
